d537d2bc1cc30b5dfc070d236b555b6c1c46f174
Ahmed Khaled <ahmedkhaled4d@gmail.com>
2025-05-16 17:40:23 +0300

    setLoading

--- src/components/products/tab.tsx
+++ src/components/products/tab.tsx
@@ -39,30 +39,30 @@ export const ProductTabs = () => {
     fetchData();
   }, []);
 
-  return (
-    <Tabs dir="rtl" defaultValue={tabs.at(0)?.value} className="w-full">
-      <TabsList className="flex">
+  if (loading) return <MyLoader />;
+  else
+    return (
+      <Tabs dir="rtl" defaultValue={tabs.at(0)?.value} className="w-full">
+        <TabsList className="flex">
+          {tabs.map((item) => (
+            <TabsTrigger key={item.value} value={item.value} className="flex-1">
+              {item.title} ({item.count})
+            </TabsTrigger>
+          ))}
+        </TabsList>
+
         {tabs.map((item) => (
-          <TabsTrigger key={item.value} value={item.value} className="flex-1">
-            {item.title} ({item.count})
-          </TabsTrigger>
+          <TabsContent key={item.value} value={item.value} className="mt-6">
+            {item.products.length > 0 && (
+              <div className="grid gap-5 grid-cols-2 sm:grid-cols-3 md:grid-cols-4">
+                {item.products.map((product) => (
+                  <ProductItem key={product.id} item={product} />
+                ))}
+              </div>
+            )}
+            {item.products.length === 0 && <ProductEmpty />}
+          </TabsContent>
         ))}
-      </TabsList>
-
-      {loading && <MyLoader />}
-
-      {tabs.map((item) => (
-        <TabsContent key={item.value} value={item.value} className="mt-6">
-          {item.products.length > 0 && (
-            <div className="grid gap-5 grid-cols-2 sm:grid-cols-3 md:grid-cols-4">
-              {item.products.map((product) => (
-                <ProductItem key={product.id} item={product} />
-              ))}
-            </div>
-          )}
-          {item.products.length === 0 && <ProductEmpty />}
-        </TabsContent>
-      ))}
-    </Tabs>
-  );
+      </Tabs>
+    );
 };
